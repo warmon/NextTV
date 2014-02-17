a0a217e3e9ace3600b8c078ec134e2253cdf3dac
apedan <alexandr.pedan@gmail.com>
2014-02-17 15:23:34 +0200

    Add/remove Favorites

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     $('#container .center-box .slider:last').addClass('last');
     $('.footer-nav ul li:last').addClass('last');
     $('.main-nav ul li:last').addClass('last');
@@ -21,108 +21,104 @@ $(document).ready(function(){
     $('.pack-box-row .pack-box:last-child').addClass('last');
     $('.order-row .my-films:last-child').addClass('last');
     $('.main-content.map ul li:last').addClass('last');
-    
-    
+
+
     //remove default input text
-    $('input[type=text], input[type=password], textarea').each(function() {
+    $('input[type=text], input[type=password], textarea').each(function () {
         var default_value = this.value;
-        $(this).focus(function() {
-            if(this.value == default_value) {
+        $(this).focus(function () {
+            if (this.value == default_value) {
                 this.value = '';
             }
         });
-        $(this).blur(function() {
-            if(this.value == '') {
+        $(this).blur(function () {
+            if (this.value == '') {
                 this.value = default_value;
             }
         });
     });
-    
+
     //$("ul.tabs").tabs("div.panes > div");
     $(".tabs-box").tabs();
-    
+
     $('.checkbox').uniform();
     $('.radio').uniform();
     $('.radio2').uniform();
     $('.radio3').uniform();
-    
+
     $(".accordion h3:first").addClass("active");
     $(".accordion p:not(:first)").hide();
- 
-    $(".accordion h3").click(function(){
- 
+
+    $(".accordion h3").click(function () {
+
         $(this).next("p").slideToggle("slow")
-        .siblings("p:visible").slideUp("slow");
+            .siblings("p:visible").slideUp("slow");
         $(this).toggleClass("active");
         $(this).siblings("h3").removeClass("active");
-     });
-     // accordion //
-     
-     $('a.up').fadeOut(0);
-     
-     $('a.up').on("click",function(){
+    });
+    // accordion //
+
+    $('a.up').fadeOut(0);
+
+    $('a.up').on("click", function () {
         var percentage = 100;
         var height = $(document).height();
         var remove = +height / +100 * +percentage;
         var spaceFromTop = +height - +remove;
-        $('html,body').animate({ scrollTop: spaceFromTop }, 'slow', function () {});
+        $('html,body').animate({ scrollTop: spaceFromTop }, 'slow', function () {
+        });
     });
 
-    
-    
-    
-    
-        
-    $('.calendar').click(function(e){
-        
+
+    $('.calendar').click(function (e) {
+
         $('.ui-datepicker').fadeIn(300);
-        
+
         $(".calendar").datepicker({
             showOtherMonths: true,
             selectOtherMonths: true
         });
-        
+
         e.stopPropagation();
-        
-        $('body').click(function(){
+
+        $('body').click(function () {
             $('.ui-datepicker').fadeOut(300);
         });
-        
-        $('.ui-datepicker').click(function(e){
-                 e.stopPropagation();
-        });
-        
-    });
-         
-         
-     // hide #back-top first
-	$(".up").hide();
-	
-	// fade in #back-top
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
-				$('.up').fadeIn();
-			} else {
-				$('.up').fadeOut();
-			}
-		});
 
-		// scroll body to 0px on click
-		$('.up').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-			return false;
-		});
-	});
-    
-         
-    
+        $('.ui-datepicker').click(function (e) {
+            e.stopPropagation();
+        });
+
+    });
+
+
+    // hide #back-top first
+    $(".up").hide();
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.up').fadeIn();
+            } else {
+                $('.up').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('.up').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+
 });
 
-$(window).load(function() {
-    
+$(window).load(function () {
+
     $('.flexslider').flexslider({
         animation: "slide",
         animationLoop: false,
@@ -130,7 +126,7 @@ $(window).load(function() {
         minItems: 5,
         maxItems: 5
     });
-    
+
     $('.flexslider2').flexslider({
         animation: "slide",
         animationLoop: false,
@@ -139,5 +135,5 @@ $(window).load(function() {
         maxItems: 3,
         itemMargin: 20
     });
-    
+
 });
